67477674141a422d61eb31a30c694b852c88228b
Yaroslav <107879647+YaroslavYanko@users.noreply.github.com>
2023-05-25 17:32:21 +0300

    Add files via upload

--- src/modules/main-page/components/Footer/Footer.jsx
+++ src/modules/main-page/components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styles from "./footer.module.css";
 import { Link } from "react-router-dom";
+import smartphone_phone from "../../../../images/icons/smartphone_phone.png";
+import letter_newsletter from "../../../../images/icons/letter_newsletter.png";
+import calendar_icon from "../../../../images/icons/calendar_icon.png";
+import secure_lock from "../../../../images/icons/secure_lock.png";
 
 function Footer() {
   return (
@@ -28,17 +32,32 @@ function Footer() {
           </nav>
           <div className={styles.work__time}>
             <h3>Графік роботи:</h3>
-            <span>Пн - Птн 10:00 ~ 18:00</span>
+            <div className={styles.work__time_boxes}>
+              <img src={calendar_icon} alt="Графік роботи" />
+              <span>Пн - Птн 10:00 ~ 18:00</span>
+            </div>
+
             <h3 className={styles.weekend__heading}>Вихідні:</h3>
-            <span>Cубота - Неділя</span>
+            <div className={styles.work__time_boxes}>
+              <img src={secure_lock} alt="Вихідні" />
+              <span>Cубота - Неділя</span>
+            </div>
           </div>
 
           <div className={styles.contacts}>
             <h3>Номер телефону:</h3>
-            <a href="tel:+380986388747">+380 98 6388 747</a>
+            <div className={styles.contacts__boxes}>
+              <img src={smartphone_phone} alt="телефон" />
+              <a href="tel:+380986388747">+380 98 6388 747</a>
+            </div>
 
             <h3 className={styles.email__heading}>Електронна пошта:</h3>
-            <a href="mailto:tvoyapolychka@gmail.com">tvoyapolychka@gmail.com</a>
+            <div className={styles.contacts__boxes}>
+              <img src={letter_newsletter} alt="пошта" />
+              <a href="mailto:tvoyapolychka@gmail.com">
+                tvoyapolychka@gmail.com
+              </a>
+            </div>
           </div>
         </div>
       </div>
